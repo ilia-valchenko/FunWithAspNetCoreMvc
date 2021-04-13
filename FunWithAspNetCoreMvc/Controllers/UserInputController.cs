0066147d2b475399a87b5baecb55ee9660344a89
Ilya Valchanka <Ilya.Valchanka@thomsonreuters.com>
2021-04-13 10:47:36 +0300

    Implemented UserInputController.

---
+++ FunWithAspNetCoreMvc/Controllers/UserInputController.cs
@@ -0,0 +1,28 @@
+﻿using FunWithAspNetCoreMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FunWithAspNetCoreMvc.Controllers
+{
+    // It's a bad controller name. Don't use it :)
+    public class UserInputController : Controller
+    {
+        public IActionResult Index()
+        {
+            // The view contains a set of user inputs.
+            return View();
+        }
+
+        // EXAMPLE #1: The action parameters names should match HTML input names.
+        public string ProceedResult(string firstname, string lastname, int age)
+        {
+            var user = new User
+            {
+                FirstName = firstname,
+                LastName = lastname,
+                Age = age
+            };
+
+            return $"Firstname: {user.FirstName}, Lastname: {user.LastName}, Age: {user.Age}.";
+        }
+    }
+}
---
+++ FunWithAspNetCoreMvc/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace FunWithAspNetCoreMvc.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
